58863435bfcfcb9eb49e4ea966b686ccf1fe9e6e
hktaylan <hktaylan@vresh.nl>
2014-04-21 17:38:08 +0200

    Excercise frame layout done

--- src/OefenFrame.java
+++ src/OefenFrame.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.*;
 
 public class OefenFrame extends JFrame {
     public OefenFrame(String naam, int groep, int aantal, int random) {
-        setSize(440, 265);
+        setSize(440, 310);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setContentPane(new OefenPaneel(this, naam, groep, aantal, random));
         setTitle("Rekentrainer - Groep: " + groep);
@@ -14,13 +15,52 @@ public class OefenFrame extends JFrame {
 
 class OefenPaneel extends JPanel implements ActionListener {
     private JFrame oefenFrame;
+    private JLabel welkomLabel, opgaveLabel, aantalGoedLabel, aantalFoutLabel, aantalTeGaanLabel;
+    private JButton volgendeKnop;
+    private JTextField invulVak;
+    private JPanel statusGroep;
+    private String naam;
+    private int groep, aantal, random, getalA, getalB, antwoordGetal, aantalGoed, aantalFout, aantalTeGaan;
 
     public OefenPaneel(JFrame oefenFrame, String naam, int groep, int aantal, int random) {
-
         this.oefenFrame = oefenFrame;
-        JLabel label = new JLabel("Naam: " + naam + "; Groep: " + groep + "; Aantal: " + aantal + "; Random: " + random);
-        add(label);
+        this.naam = naam;
+        this.groep = groep;
+        this.aantal = aantal;
+        this.random = random;
+        setLayout(null);
 
+        welkomLabel = new JLabel("Welkom " + naam + ", vul het antwoord van de volgende som in?");
+        welkomLabel.setFont(new Font("Arial", Font.BOLD, 13));
+        welkomLabel.setBounds(40, 20, 450, 20);
+        add(welkomLabel);
+
+        opgaveLabel = new JLabel(getalA + "   +   " + getalB + "   = ");
+        opgaveLabel.setFont(new Font("Arial", Font.BOLD, 48));
+        opgaveLabel.setBounds(80, 80, 260, 45);
+        add(opgaveLabel);
+
+        invulVak = new JTextField(2);
+        invulVak.setBounds(320, 80, 65, 45);
+        add(invulVak);
+
+        volgendeKnop = new JButton("Volgende som");
+        volgendeKnop.setBounds(100, 150, 270, 30);
+        add(volgendeKnop);
+
+        statusGroep = new JPanel();
+        statusGroep.setBounds(5, 200, 430, 85);
+        statusGroep.setLayout(new GridLayout(3, 1));
+        statusGroep.setBorder(new EmptyBorder(5, 5, 5, 5));
+        statusGroep.setBackground(Color.LIGHT_GRAY);
+        add(statusGroep);
+
+        aantalGoedLabel = new JLabel("Aantal sommen tot nu toe goed: " + aantalGoed);
+        statusGroep.add(aantalGoedLabel);
+        aantalFoutLabel = new JLabel("Aantal sommen tot nu toe fout: " + aantalFout);
+        statusGroep.add(aantalFoutLabel);
+        aantalTeGaanLabel = new JLabel("Nog " + aantalTeGaan + " sommen te maken");
+        statusGroep.add(aantalTeGaanLabel);
     }
 
     public void actionPerformed(ActionEvent e) {
